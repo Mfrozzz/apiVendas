7aa6ec30afee9dbd957dfe0a22f81332ac84f2b4
Mfrozzz <marcosvinibva@gmail.com>
2022-11-30 15:54:07 -0300

    first commit

---
+++ src/errors/AppError.ts
@@ -0,0 +1,9 @@
+export default class AppError{
+    public readonly messege: string;
+    public readonly statusCode: number;
+
+    constructor(message:string,statusCode=400){
+        this.messege=message;
+        this.statusCode = statusCode;
+    }
+}
