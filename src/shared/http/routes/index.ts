535dcf2f5d0bb3ef554ab7c6c9632e31df017e58
Mfrozzz <marcosvinibva@gmail.com>
2023-02-27 15:28:55 -0300

    customer's module

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customerRouter from "@modules/customers/routes/customer.routes";
 import productRouter from "@modules/products/routes/products.routes";
 import passwordRouter from "@modules/users/routes/password.routes";
 import profileRouter from "@modules/users/routes/profile.routes";
@@ -12,6 +13,7 @@ routes.use('/users',userRouter);
 routes.use('/sessions',sessionsRouter);
 routes.use('/password',passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers',customerRouter);
 
 /*routes.get("/",(request,response)=>{
     return response.json({message:'Hello Dev!'});
